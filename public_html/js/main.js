005203b5477eccfc24c05b7da85f60761a45cfb1
caiocsn <caiocsn@hotmail.com>
2014-05-28 14:29:06 -0300

    28-05

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -10,6 +10,6 @@ function changeContent(loc){
 			$('iframe').delay(200).animate({ "left": "-100%" }, "slow", function(){
 			document.getElementById("content-frame").src= loc;
 			document.getElementById("content-frame").style.left = "100%";
-			$('iframe').delay(200).animate({ "left": "0%" }, "slow" );;
+			$('iframe').delay(200).animate({ "left": "20%" }, "slow" );;
 		});
 	}
