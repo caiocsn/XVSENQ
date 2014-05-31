201f3d1db2b62dcdcbaea6bc0f90569be2c17848
caiocsn <caiocsn@hotmail.com>
2014-05-31 06:37:01 -0300

    31- 05

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -7,9 +7,9 @@
 
 function changeContent(loc){
 			
-			$('iframe').delay(200).animate({ "left": "-100%" }, "slow", function(){
+			$('iframe').delay(200).animate({ "left": "-80%" }, "slow", function(){
 			document.getElementById("content-frame").src= loc;
 			document.getElementById("content-frame").style.left = "100%";
-			$('iframe').delay(200).animate({ "left": "20%" }, "slow" );;
+			$('iframe').delay(200).animate({ "left": "10%" }, "slow" );;
 		});
 	}
